5e51ec3d28a9129c1da63b87133458f5e26abacb
Lirixpro <lirix1@163.com>
2025-07-14 03:42:03 -0700

    Site updated: 2025-07-14 03:41:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["hello-world/","ai-com/"];function toRandomPost(){
+var posts=["hello-world/","ai-com/","hexo-blog-building/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
